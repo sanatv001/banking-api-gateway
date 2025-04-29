87d9b0a4e092415e74d1ab6045729d0c5e138a63
sanatv001 <sanatv208@gmail.com>
2025-04-29 14:47:51 +0530

    Initial commit

---
+++ src/main/java/com/sv/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,20 @@
+package com.sv.api_gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public KeyResolver ipKeyResolver(){
+        return exchange -> Mono.just(
+                exchange.getRequest()
+                        .getRemoteAddress()
+                        .getAddress()
+                        .getHostName()
+        );
+    }
+}

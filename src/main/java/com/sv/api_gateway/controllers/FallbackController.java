0aa9e6333d7bab64c1d6b390585afd0419182a9f
sanatv001 <sanatv208@gmail.com>
2025-04-30 15:00:53 +0530

    Added fallback response

---
+++ src/main/java/com/sv/api_gateway/controllers/FallbackController.java
@@ -0,0 +1,16 @@
+package com.sv.api_gateway.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fallback")
+public class FallbackController {
+
+    @RequestMapping(value = "/account", method = {RequestMethod.GET, RequestMethod.POST})
+    public String accountServiceFallback(){
+        return "Account Service is currently unavailable. Please try again later.";
+    }
+}
